182a7ca3c8e280a662993d1e3be7648ac42e4752
irenfeliz <irenfeliz@gmail.com>
2022-09-11 11:17:07 +0200

    Initial Commit

---
+++ FindNumber/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  FindNumber
+//
+//  Created by Irina Zorina on 05.09.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
